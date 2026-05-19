dec0fe4fd3903a3b15ac961cb0ca5ca6cefa37bf
AmritPrashantSingh18 <amritprashant.singh@gmail.com>
2026-05-20 01:26:42 +0530

    ForLoop

---
+++ src/SumOfNosUsingForLoops.java
@@ -0,0 +1,15 @@
+public class SumOfNosUsingForLoops {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+                count++;
+            }
+        }
+        System.out.println("Sum is " + sum);
+        System.out.println("Count of it divided by 3 is " + count);
+    }
+}
