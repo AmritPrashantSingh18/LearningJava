14ca6a6408147585b9b241504cc11997151dc454
AmritPrashantSingh18 <amritprashant.singh@gmail.com>
2026-05-30 01:24:18 +0530

    Upper Case

---
+++ src/UpperCaseLettersInString.java
@@ -0,0 +1,16 @@
+public class UpperCaseLettersInString {
+
+    void upperCaseInString(String a) {
+        char[] ch = a.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] >= 'A' && ch[i] <= 'Z') {
+                System.out.println(ch[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        UpperCaseLettersInString uc = new UpperCaseLettersInString();
+        uc.upperCaseInString("AmRIT PrashaNT SIngh");
+    }
+}
