8a6daff12cbe63bdc26fe9649673edb614835c5e
AmritPrashantSingh18 <amritprashant.singh@gmail.com>
2026-06-12 03:40:38 +0530

    Practice

---
+++ src/SmallestNumber.java
@@ -0,0 +1,53 @@
+public class SmallestNumber {
+
+    public static int smallestInteger(int a, int b, int c) {
+        if ((a < b) && (a < c)) {
+            return a;
+        } else if ((b < a) && (b < c)) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static String gradeMarks(double a) {
+        if (a >= 80 && a <= 100) {
+            return "A";
+        } else if (a < 80 && a >= 60) {
+            return "B";
+        } else if (a < 60 && a >= 50) {
+            return "C";
+        } else if (a < 50 && a >= 45) {
+            return "D";
+        } else if (a < 45 && a >= 25) {
+            return "E";
+        } else if (a < 25 && a >= 0) {
+            return "F";
+        } else {
+            return "invalid";
+        }
+    }
+
+    public static boolean compareNumber(int a, int b, int c) {
+        if ((a == b) || (b == c)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(smallestInteger(3, 1, 2));
+        System.out.println(gradeMarks(80));
+        System.out.println(gradeMarks(79));
+        System.out.println(gradeMarks(59));
+        System.out.println(gradeMarks(49));
+        System.out.println(gradeMarks(35));
+        System.out.println(gradeMarks(24));
+        System.out.println(gradeMarks(0));
+        System.out.println(gradeMarks(-1));
+        System.out.println(compareNumber(10,10,20));
+        System.out.println(compareNumber(10,20,20));
+        System.out.println(compareNumber(10,30,20));
+    }
+}
